288ca44bd0faf97e90e86893557a15b7fd8a1d17
Samuel Spurling <samuelspurling@googlemail.com>
2018-03-10 19:15:55 +0000

    updated script to say yes automatically. added monokai terminal theme

--- setup.sh
+++ setup.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 # Update and upgrade #
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 # Remove unrequired apps (except 'Mines') #
-sudo apt remove aisleriot gnome-mahjongg gnome-sudoku 
+sudo apt remove -y aisleriot gnome-mahjongg gnome-sudoku 
 # Install new apps #
-sudo apt install python2.7 python-pip
-sudo apt-get install git
-sudo apt-get install vim
+sudo apt install -y python2.7 python-pip
+sudo apt-get -y install git
+sudo apt-get -y install vim
 # Install vim configs #
-sudo apt-get install build-essential cmake
+sudo apt-get -y install build-essential cmake
 git clone --recurse-submodules https://github.com/samuelspurling/.vim ~/.vim
 python ~/.vim/bundle/YouCompleteMe/install.py
+# Change terminal colors to be not horrible #
+mkdir ~/temp
+git clone git://github.com/pricco/gnome-terminal-colors-monokai.git ~/temp/gnome-terminal-colors-monokai
+~/temp/gnome-terminal-colors-monokai/install.sh
+rm -r ~/temp
